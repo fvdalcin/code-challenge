86b048923e1f945565857cbd0c635066b9bd0243
GLOBAL\I843892 <felipe.dalcin@sap.com>
2017-07-11 16:02:15 -0300

    Initial list view created

--- challenge1/app/src/main/java/dalcin/bearch/mobilechallenge/RepositoryListActivity.java
+++ challenge1/app/src/main/java/dalcin/bearch/mobilechallenge/RepositoryListActivity.java
@@ -1,13 +1,30 @@
 package dalcin.bearch.mobilechallenge;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class RepositoryListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class RepositoryListActivity extends Activity {
+
+    private ListView mRepositoryListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repository_list);
+
+        mRepositoryListView = (ListView) findViewById(R.id.repository_list);
+        ArrayList<String> repositoryItems = new ArrayList<String>();
+        repositoryItems.add("Item 1");
+        repositoryItems.add("Item 2");
+        repositoryItems.add("Item 3");
+        repositoryItems.add("Item 4");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, repositoryItems);
+        mRepositoryListView.setAdapter(adapter);
     }
 }
